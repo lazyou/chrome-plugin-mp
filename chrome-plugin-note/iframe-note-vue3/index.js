a9e3823c96d5bd586bc52e3326fb0474a5588f2b
lazyou <1394139539@qq.com>
2023-03-06 18:00:25 +0800

    feat: vue chrome 执行案例

--- chrome-plugin-note/iframe-note-vue3/index.js
+++ chrome-plugin-note/iframe-note-vue3/index.js
@@ -1,12 +1,33 @@
 window.onload = function() {
     const App = {
-        data() {
+        data: () => {
             return {
-                message: "Hello Element Plus",
+                text: "",
+                textStatus: true,
             };
         },
+        created() {
+            this.init();
+        },
+        methods: {
+            init() {
+                console.log('textToggle');
+                // 执行chrome脚本
+                chrome.tabs.executeScript({
+                    code:"$('body').prepend(`<h1>页面新增内容</h1>`)"
+                });
+            },
+            textToggle() {
+                console.log('textToggle');
+                this.textStatus = !this.textStatus;
+            },
+            textChange() {
+                console.log('textChange');
+                console.log(this.text)
+            },
+        }
     };
     const app = Vue.createApp(App);
     app.use(ElementPlus);
-    app.mount("#app");
+    app.mount("#vue-app");
 }
--- chrome-plugin-note/js/content-script.js
+++ chrome-plugin-note/js/content-script.js
@@ -8,8 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // 嵌入html
     const iFrame = document.createElement("iframe");
-    // iFrame.src  = chrome.extension.getURL ("iframe-note-jquery/index.html");
+    iFrame.id = "vue-iframe";
+    iFrame.style= `
+        height: auto; 
+        width: auto; 
+        min-height: 20px; 
+        min-width: 80px;
+        position: fixed;
+        z-index: 99999;
+        right: 0;
+        border-bottom-left-radius: 4px;
+        border-bottom-right-radius: 4px;
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+    `;
     iFrame.src  = chrome.extension.getURL ("iframe-note-vue3/index.html");
+    // iFrame.src  = chrome.extension.getURL ("iframe-note-jquery/index.html");
     document.body.insertBefore(iFrame, document.body.firstChild);
 
     // 从页面拿数据
