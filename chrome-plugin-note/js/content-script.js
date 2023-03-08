4ae154d461c33d8013a0724746121b08d6aaaab7
lazyou <1394139539@qq.com>
2023-03-08 14:37:52 +0800

    vue3 在 popup 内实现笔记、storage按域名缓存

--- chrome-plugin-note/js/content-script.js
+++ chrome-plugin-note/js/content-script.js
@@ -1,41 +1,25 @@
 console.log('content-script.js');
 
-
 // 注意，必须设置了 run_at=document_start 此段代码才会生效
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('content-script DOMContentLoaded!');
-    console.log(window.location.href);
+    console.log('content-script.js DOMContentLoaded!', window.location.href);
 
-    // 嵌入html
-    const iFrame = document.createElement("iframe");
-    iFrame.id = "vue-iframe";
-    iFrame.style= `
-        height: 300px; 
-        width: 300px;
-        position: fixed;
-        z-index: 99999;
-        right: 0;
-        border-radius: 4px;
-        border: 1px solid black;
-        resize: vertical;
-        overflow: auto;
-    `;
-    iFrame.src  = chrome.extension.getURL ("iframe-note-vue3/index.html");
-    // iFrame.src  = chrome.extension.getURL ("iframe-note-jquery/index.html");
-    document.body.insertBefore(iFrame, document.body.firstChild);
-
-
-    // // 从页面拿数据
-    // let url = $('#_url').val();
-    // let cookiesStr = $('#_cookies').val();
-    // if (url && cookiesStr) {
-    //     let cookies = JSON.parse(cookiesStr);
-    //     chrome.runtime.sendMessage({cookies: cookies, redirectURL: url}, (response) => {
-    //         console.log('reciver background.js：');
-    //         console.log(response);
-    //     });
-    // } else {
-    //     console.log('no cookies');
-    // }
+    // // 嵌入 vue3 | jquery 的 iframe html
+    // const iFrame = document.createElement("iframe");
+    // iFrame.id = "vue-iframe";
+    // iFrame.style= `
+    //     height: 300px;
+    //     width: 300px;
+    //     position: fixed;
+    //     z-index: 99999;
+    //     right: 0;
+    //     border-radius: 4px;
+    //     border: 1px solid black;
+    //     resize: vertical;
+    //     overflow: auto;
+    // `;
+    // iFrame.src  = chrome.extension.getURL ("iframe-note-vue3/index.html");
+    // // iFrame.src  = chrome.extension.getURL ("iframe-note-jquery/index.html");
+    // document.body.insertBefore(iFrame, document.body.firstChild);
 });
 
---
+++ chrome-plugin-note/js/vue_index.js
@@ -0,0 +1,78 @@
+window.onload = function() {
+    const App = {
+        data: () => {
+            return {
+                text: "",
+                textStatus: true,
+                tabHost: '', // 当前标签域名
+            };
+        },
+        created() {
+            this.init()
+            this.tabHostInit()
+            this.textInitDelay()
+        },
+        watch: {
+            // 每当 question 改变时，这个函数就会执行
+            tabHost(newTabHost, oldTabHost) {
+                console.log('newTabHost tabHost:', newTabHost)
+                console.log('oldTabHost tabHost:', oldTabHost)
+            }
+        },
+        methods: {
+            init() {
+                console.log('init')
+                createContextMenus()
+
+                // // 在vue执行执行chrome脚本
+                // chrome.tabs.executeScript({
+                //     code:"$('body').prepend(`<h1>页面新增内容</h1>`)"
+                // })
+            },
+            tabHostInit() {
+                chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+                    let url = tabs[0].url
+                    let parseUrl = new URL(url)
+                    this.tabHost = parseUrl.host
+                })
+            },
+            // 根据当前tab url，从 storage 里取笔记缓存
+            textInitDelay() {
+                setTimeout(() => {
+                    chrome.storage.sync.get(this.getStorageObj(), (saveObj) => {
+                        this.text = saveObj[this.tabHost] ?? ''
+                    })
+                }, 100)
+            },
+            // 根据当前tab url，将 笔记缓存 存入 storage 里
+            textSave() {
+                if (this.text) {
+                    chrome.storage.sync.set(this.getStorageObj(), function() {
+                        console.log('textSave: 保存成功！')
+                    })
+                } else {
+                    console.log('textSave: text 为空，不保存')
+                }
+            },
+            textToggle() {
+                this.textStatus = !this.textStatus;
+            },
+            textChange() {
+                this.textSave()
+            },
+            getTabHost() {
+                return this.tabHost ?? 'unknown.com'
+            },
+            getStorageObj() {
+                let saveObj = {}
+                saveObj[this.getTabHost()] = this.text
+
+                return saveObj;
+            }
+        }
+    };
+
+    const app = Vue.createApp(App)
+    app.use(ElementPlus)
+    app.mount("#vue-app")
+}
